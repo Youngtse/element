aaac7ec88151c4aacd4370823ef6b38e4aa22d2a
YangGen <summer.yang.1109@gmail.com>
2018-10-31 17:23:38 +0800

    revert

--- packages/table/src/table-column.js
+++ packages/table/src/table-column.js
@@ -100,9 +100,6 @@ const DEFAULT_RENDER_CELL = function(h, { row, column }) {
   const value = property && property.indexOf('.') === -1
     ? row[property]
     : getValueByPath(row, property);
-  if (!value) {
-    return property;
-  }
   if (column && column.formatter) {
     return column.formatter(row, column, value);
   }
@@ -267,29 +264,28 @@ export default {
 
     column.renderCell = function(h, data) {
       // 未来版本移除
-      // if (_self.$vnode.data.inlineTemplate) {
-      //   renderCell = function() {
-      //     data._self = _self.context || data._self;
-      //     if (Object.prototype.toString.call(data._self) === '[object Object]') {
-      //       for (let prop in data._self) {
-      //         if (!data.hasOwnProperty(prop)) {
-      //           data[prop] = data._self[prop];
-      //         }
-      //       }
-      //     }
-      //     // 静态内容会缓存到 _staticTrees 内，不改的话获取的静态数据就不是内部 context
-      //     data._staticTrees = _self._staticTrees;
-      //     data.$options.staticRenderFns = _self.$options.staticRenderFns;
-      //     return _self.customRender.call(data);
-      //   };
-      // } else if (_self.$scopedSlots.default) {
-      //   renderCell = () => _self.$scopedSlots.default(data);
-      // }
-      //
-      // if (!renderCell) {
-      //   renderCell = DEFAULT_RENDER_CELL;
-      // }
-      renderCell = DEFAULT_RENDER_CELL;
+      if (_self.$vnode.data.inlineTemplate) {
+        renderCell = function() {
+          data._self = _self.context || data._self;
+          if (Object.prototype.toString.call(data._self) === '[object Object]') {
+            for (let prop in data._self) {
+              if (!data.hasOwnProperty(prop)) {
+                data[prop] = data._self[prop];
+              }
+            }
+          }
+          // 静态内容会缓存到 _staticTrees 内，不改的话获取的静态数据就不是内部 context
+          data._staticTrees = _self._staticTrees;
+          data.$options.staticRenderFns = _self.$options.staticRenderFns;
+          return _self.customRender.call(data);
+        };
+      } else if (_self.$scopedSlots.default) {
+        renderCell = () => _self.$scopedSlots.default(data);
+      }
+
+      if (!renderCell) {
+        renderCell = DEFAULT_RENDER_CELL;
+      }
 
       return _self.showOverflowTooltip || _self.showTooltipWhenOverflow
         ? <div class="cell el-tooltip" style={'width:' + (data.column.realWidth || data.column.width) + 'px'}>{ renderCell(h, data) }</div>
